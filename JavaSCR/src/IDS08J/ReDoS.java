ab1b840ae873f7b80af73f515adb7621f4a47a55
rcseacord <rcseacord@gmail.com>
2017-02-08 17:02:18 -0500

    added another ReDoS example

--- JavaSCR/src/IDS08J/ReDoS.java
+++ JavaSCR/src/IDS08J/ReDoS.java
@@ -4,16 +4,21 @@ public class ReDoS {
 	public static void main(String[] args) {
 
 		final String pattern = "(aa|aab?)*";
-
-		// two 'a'
-		final String a002 = "aa";
-		// three 'a'
-		final String a003 = "aaa";
+		final String a002 = "aa"; // two 'a'
+		final String a003 = "aaa";  // three 'a'
 		// 102 'a'
 		final String a102 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 		// 103 'a'
 		final String a103 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
+		final String ax4 = "aaaaX";	
+		final String ax16 = "aaaaaaaaaaaaaaaaX";
+		final String ax32 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX";
+		
+		System.out.println(ax4.matches("^(a+)+$"));
+		System.out.println(ax16.matches("^(a+)+$"));
+		System.out.println(ax32.matches("^(a+)+$"));
+		
 		System.out.println(a002.matches(pattern));
 		System.out.println(a003.matches(pattern));
 		System.out.println(a102.matches(pattern));
