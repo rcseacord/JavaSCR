1370c92b672033d5fd807b77982bb62e7fccb673
rcseacord <rcseacord@gmail.com>
2017-05-10 13:18:55 +0100

    removed some defects

--- JavaSCR/src/ser04j/Hometown.java
+++ JavaSCR/src/ser04j/Hometown.java
@@ -33,7 +33,7 @@ import java.nio.file.AccessDeniedException;
 
 public final class Hometown implements Serializable {
   private static final long serialVersionUID = 6515419803685137985L;
-  // Private internal state 
+  // Private internal state
   private String town;
   private static final String UNKNOWN = "UNKNOWN";
   private static final String MOSCOW = "Moscow";
@@ -45,24 +45,22 @@ public final class Hometown implements Serializable {
   }
 
   void validateInput(String newCC) throws IllegalArgumentException {
-    // ... 
+    // ...
   }
 
-  public Hometown(String town) {
-    try {
-      performSecurityManagerCheck(town);
-    } catch (AccessDeniedException e) {
-      e.printStackTrace();
-    }
-
+  public Hometown(String town) throws AccessDeniedException {
+    performSecurityManagerCheck(town);
+    validateInput(town);
     // Initialize town to default value
     this.town = town;
   }
 
   // Allows callers to retrieve internal state without security check
-  String getTown() { return town; }
+  String getTown() {
+    return town;
+  }
 
-  // Privileged  callers can modify (private) state
+  // Privileged callers can modify (private) state
   public void setTown(String newTown) throws AccessDeniedException {
     if (town.equals(newTown)) {
       // No change
@@ -75,29 +73,25 @@ public final class Hometown implements Serializable {
   }
 
   private void writeObject(ObjectOutputStream out) throws IOException {
-    //out.writeObject(town);
-  out.defaultWriteObject();
+    // out.writeObject(town);
+    out.defaultWriteObject();
   }
 
-  private void readObject(ObjectInputStream in) throws IOException {
+  private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
     String town;
-    try {
-      ObjectInputStream.GetField fields = in.readFields();
-      town = (String)fields.get("town", null);
-    } catch (ClassNotFoundException e) {
-      town = null;
-      e.printStackTrace();
-    }
+    ObjectInputStream.GetField fields = in.readFields();
+    town = (String) fields.get("town", null);
     // If the deserialized name does not match the default value normally
     // created at construction time, duplicate the checks
     if (!UNKNOWN.equals(town)) {
-      validateInput(town);
+      performSecurityManagerCheck(town); 
+      validateInput(town); 
     }
 
     this.town = town;
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws AccessDeniedException {
     Hometown myTown = new Hometown("Pittsburgh");
     System.out.println("My town is " + myTown.getTown());
 
@@ -109,18 +103,17 @@ public final class Hometown implements Serializable {
       ObjectOutputStream oos = new ObjectOutputStream(fos);
       oos.writeObject(ht);
       oos.close();
-    }
-    catch (IOException e) {
+    } catch (IOException e) {
       e.printStackTrace(System.err);
     }
 
     // Construct a new object through deserialization
     try {
-      // NOTE:  File is in %userprofile%\git\JavaSCR\JavaSCR
+      // NOTE: File is in %userprofile%\git\JavaSCR\JavaSCR
       FileInputStream fis = new FileInputStream("tempdata.ser");
       ObjectInputStream ois = new ObjectInputStream(fis);
       Hometown ht = (Hometown) ois.readObject();
-      ois.close(); 
+      ois.close();
       System.out.println("My town is " + ht.getTown());
 
       // Clean up the file
@@ -153,9 +146,22 @@ public final class Hometown implements Serializable {
 
 
 
-/* add performSecurityManagerCheck at line 94
-if (!UNKNOWN.equals(town)) {
-  performSecurityManagerCheck(town);
-  validateInput(town);
-}
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * add performSecurityManagerCheck at line 94 if (!UNKNOWN.equals(town)) {
+ * performSecurityManagerCheck(town); validateInput(town); }
+ */
