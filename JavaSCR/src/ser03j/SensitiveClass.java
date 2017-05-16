4947e63ad351af72f50a66a8de101636e2223924
rcseacord <rcseacord@gmail.com>
2017-05-16 16:10:29 -0400

    made some improvements

--- JavaSCR/src/ser03j/SensitiveClass.java
+++ JavaSCR/src/ser03j/SensitiveClass.java
@@ -1,30 +1,27 @@
 package ser03j;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InvalidClassException;
-import java.io.NotSerializableException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.InvalidObjectException;
 import java.io.ObjectStreamException;
 
-public class SensitiveClass extends Number {
+public final class SensitiveClass extends Number {
   private static final long serialVersionUID = 1L;
   private static final SensitiveClass INSTANCE = new SensitiveClass();
+
   public static SensitiveClass getInstance() {
     return INSTANCE;
   }
- 
+
   private SensitiveClass() {
     // Perform security checks and parameter validation
   }
- 
+
   private int balance = 1000;
+
   protected int getBalance() {
     return balance;
   }
-  
+
   @Override
   public double doubleValue() {
     // TODO Auto-generated method stub
@@ -48,8 +45,7 @@ public class SensitiveClass extends Number {
     // TODO Auto-generated method stub
     return 0;
   }
-
-
+  
 } // end class SensitiveClass
 
 
@@ -86,19 +82,23 @@ public class SensitiveClass extends Number {
 
 
 
-/*
-// Cause deserialization to fail
-private void writeObject(java.io.ObjectOutputStream out) throws IOException {
-  throw new IOException("Not serializable");
-}
-private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
-  throw new IOException("Not serializable");
-}
-*/
 
-/* Allow deserialization to succeed securely
-  private Object readResolve( ) {
-    // Accept no substitutes!
-    return INSTANCE;
+
+
+
+/*
+ // Cause deserialization to fail
+  private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+    throw new IOException("Not serializable");
   }
-*/
+  
+  private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+    throw new IOException("Not serializable");
+  }
+  
+  @SuppressWarnings("unused")
+  private void readObjectNoData() throws ObjectStreamException {
+     throw new InvalidObjectException("Not serializable");
+  }
+  
+ */
