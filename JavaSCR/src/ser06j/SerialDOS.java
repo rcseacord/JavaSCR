45f392718f3f740184e99782e21ef39e270b4aff
rcseacord <rcseacord@gmail.com>
2017-05-17 14:28:02 -0400

    eliminated explicit type arguments

--- JavaSCR/src/ser06j/SerialDOS.java
+++ JavaSCR/src/ser06j/SerialDOS.java
@@ -19,12 +19,12 @@ public class SerialDOS {
   }
 
   static byte[] payload() throws IOException {
-    Set<Object> root = new HashSet<Object>();
+    Set<Object> root = new HashSet<>();
     Set<Object> s1 = root;
-    Set<Object> s2 = new HashSet<Object>();
+    Set<Object> s2 = new HashSet<>();
     for (int i = 0; i < 100; i++) {
-      Set<Object> t1 = new HashSet<Object>();
-      Set<Object> t2 = new HashSet<Object>();
+      Set<Object> t1 = new HashSet<>();
+      Set<Object> t2 = new HashSet<>();
       t1.add("foo"); // make it not equal to t2
       s1.add(t1);
       s1.add(t2);
