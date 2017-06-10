3c9c12bf5482282dee29659c4626b8ddad36bb40
rcseacord <rcseacord@gmail.com>
2017-06-10 10:22:51 -0400

    quited static analysis warnings

--- JavaSCR/src/err00j/MethodCallStack.java
+++ JavaSCR/src/err00j/MethodCallStack.java
@@ -5,51 +5,51 @@ import java.lang.UnsupportedOperationException;
 class MethodCallStack {
 
 	public static void main(String[] args) {
-		System.out.println("Enter main()");
+		System.out.println("Enter main()"); //$NON-NLS-1$
 		try {
 		  methodA();
 		}
 		catch (UnsupportedOperationException uoe) {
 			uoe.printStackTrace();
 		}
-		System.out.println("Exit main()");
+		System.out.println("Exit main()"); //$NON-NLS-1$
 	}
 
 	private static void methodA() {
-		System.out.println("Enter methodA()");
+		System.out.println("Enter methodA()"); //$NON-NLS-1$
 		try {
 			  methodB();
 			}
 			finally {
-				System.out.println("methodA() finally block");
+				System.out.println("methodA() finally block"); //$NON-NLS-1$
 			}
-		System.out.println("Exit methodA()");
+		System.out.println("Exit methodA()"); //$NON-NLS-1$
 	}
 
 	private static void methodB() {
-		System.out.println("Enter methodB()");
+		System.out.println("Enter methodB()"); //$NON-NLS-1$
 		try {
 			  methodC();
 			}
 			finally {
-				System.out.println("methodB() finally block");
+				System.out.println("methodB() finally block"); //$NON-NLS-1$
 			}
-		System.out.println("Exit methodB()");
+		System.out.println("Exit methodB()"); //$NON-NLS-1$
 	}
 
 	private static void methodC() {
-		System.out.println("Enter methodC()");
+		System.out.println("Enter methodC()"); //$NON-NLS-1$
 		try {
 		  methodD();
 		}
 		finally {
-			System.out.println("methodC() finally block");
+			System.out.println("methodC() finally block"); //$NON-NLS-1$
 		}
-		System.out.println("Exit methodC()");
+		System.out.println("Exit methodC()"); //$NON-NLS-1$
 	}
 	
 	private static void methodD() {
-		System.out.println("Enter methodD()");
+		System.out.println("Enter methodD()"); //$NON-NLS-1$
 		throw new UnsupportedOperationException();
 		// unreachable
 	}
