8e42f4fce1e5695a5bfe0f487416633697bb71c3
rcseacord <rcseacord@gmail.com>
2017-08-29 15:46:47 -0400

    cleaned up a little bit

--- JavaSCR/src/ser03j/Malicious.java
+++ JavaSCR/src/ser03j/Malicious.java
@@ -1,3 +1,25 @@
+// The MIT License (MIT)
+// 
+// Copyright (c) 2017 Robert C. Seacord
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package ser03j;
 
 import java.io.ByteArrayInputStream;
@@ -7,8 +29,15 @@ import java.io.ObjectOutputStream;
 
 class Malicious {
   
-  // This method should not be used in production code
-  static public Object deepCopy(Object obj) {
+  /**
+   * Copies a specified Object by serializing then deserializing the object. 
+   * <p>
+   * This method should not be used in production code.
+   *
+   * @param  obj  the Object to copy
+   * @return      copy of obj
+    */
+  static public Object serialCopy(Object obj) {
     try {
       ByteArrayOutputStream bos = new ByteArrayOutputStream();
       new ObjectOutputStream(bos).writeObject(obj);
@@ -22,10 +51,10 @@ class Malicious {
   
   public static void main(String[] args) {
     SensitiveClass sc =
-       (SensitiveClass) deepCopy(SensitiveClass.getInstance());
+       (SensitiveClass) serialCopy(SensitiveClass.getInstance());
     // Prints false; indicates new instance
     System.out.println(sc == SensitiveClass.getInstance()); 
-    System.out.println("Balance = " + sc.getBalance()); //$NON-NLS-1$
+    System.out.println("Balance = " + sc.getBalance()); 
   }
  
 }
--- JavaSCR/src/ser03j/SensitiveClass.java
+++ JavaSCR/src/ser03j/SensitiveClass.java
@@ -1,3 +1,25 @@
+// The MIT License (MIT)
+// 
+// Copyright (c) 2017 Robert C. Seacord
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package ser03j;
 
 import java.io.IOException;
@@ -24,25 +46,21 @@ public final class SensitiveClass extends Number {
 
   @Override
   public double doubleValue() {
-    // TODO Auto-generated method stub
     return 0;
   }
 
   @Override
   public float floatValue() {
-    // TODO Auto-generated method stub
     return 0;
   }
 
   @Override
   public int intValue() {
-    // TODO Auto-generated method stub
     return 0;
   }
 
   @Override
   public long longValue() {
-    // TODO Auto-generated method stub
     return 0;
   }
   
@@ -86,19 +104,27 @@ public final class SensitiveClass extends Number {
 
 
 
-/*
- // Cause deserialization to fail
-  private void writeObject(java.io.ObjectOutputStream out) throws IOException {
-    throw new IOException("Not serializable");
-  }
-  
-  private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
-    throw new IOException("Not serializable");
-  }
-  
-  @SuppressWarnings("unused")
-  private void readObjectNoData() throws ObjectStreamException {
-     throw new InvalidObjectException("Not serializable");
-  }
-  
- */
+
+//  /**
+//   * @param out not used, but required to match signature
+//   */
+//  @SuppressWarnings("static-method")
+//  private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+//    throw new IOException("Not serializable");
+//  }
+//  
+//  /**
+//   * @param in not used, but required to match signature 
+//   * @throws IOException to prevent deserialization
+//   * @throws ClassNotFoundException not used, but required to match signature
+//   */
+//  @SuppressWarnings("static-method")
+//  private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+//    throw new IOException("Not deserializable");
+//  }
+//  
+//  @SuppressWarnings({ "unused", "static-method" })
+//  private void readObjectNoData() throws ObjectStreamException {
+//     throw new InvalidObjectException("Not deserializable");
+//  }
+
