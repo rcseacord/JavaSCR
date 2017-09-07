9dba4afcf02f59a86501d76393c11d64df81f403
rcseacord <rcseacord@gmail.com>
2017-09-06 21:27:22 -0400

    New demo and exercise

--- JavaSCR/src/ser08j/MutablePeriod.java
+++ JavaSCR/src/ser08j/MutablePeriod.java
@@ -1,55 +1,79 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2017 Robert C. Seacord
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package ser08j;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Date;
 
 public class MutablePeriod {
   // A period instance
-  public final Period period;
+  public Period period = null;
   // period's start field, to which we shouldn't have access
-  public final Date start;
+  public Date start = null;
   // period's end field, to which we shouldn't have access
-  public final Date end;
+  public Date end = null;
 
   public MutablePeriod() {
-    try {
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      ObjectOutputStream out = new ObjectOutputStream(bos);
+    try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);) {
       // Serialize a valid Period instance
-      out.writeObject(new Period(new Date(), new Date()));
-      /*
-       * Append rogue "previous object refs" for internal Date fields in Period.
-       * For details, see "Java Object Serialization Specification," Section
-       * 6.4.
-       */
-      byte[] ref = { 0x71, 0, 0x7e, 0, 5 }; // Ref #5
-      bos.write(ref); // The start field
-      ref[4] = 4; // Ref # 4
-      bos.write(ref); // The end field
+      oos.writeObject(new Period(new Date(), new Date()));
 
-      // Deserialize Period and "stolen" Date references
-      ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bos.toByteArray()));
-      this.period = (Period) in.readObject();
-      this.start = (Date) in.readObject();
-      this.end = (Date) in.readObject();
-    } catch (Exception e) {
-      throw new AssertionError(e);
+      // Append rogue previous object references for internal Date fields in Period.
+      byte[] ref = { 0x71,  0x00, 0x7e, 0x00, 0x05 }; // Reference #5
+      baos.write(ref); // The start field
+      ref[4] = 0x04; // Reference # 4
+      baos.write(ref); // The end field
+
+      try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()))) {
+        // Deserialize Period and "stolen" Date references
+        this.period = (Period) ois.readObject();
+        this.start = (Date) ois.readObject();
+        this.end = (Date) ois.readObject();
+      } catch (ClassNotFoundException | IOException e) {
+        e.printStackTrace();
+      } 
+    } // end try-with-resources
+    catch (IOException e1) {
+      e1.printStackTrace();
     }
-  } // end ctor
+  } // end MutablePeriod constructor
 
   @SuppressWarnings("deprecation")
   public static void main(String[] args) {
+    System.out.println("MutablePeriod");
     MutablePeriod mp = new MutablePeriod();
     Period p = mp.period;
     Date pEnd = mp.end;
-    // Let's turn back the clock   
-    pEnd.setYear(78);   
-    System.out.println(p);   
-    // Bring back the 60s!    
-    pEnd.setYear(69);    
+    // Let's turn back the clock
+    pEnd.setYear(78);
     System.out.println(p);
-    }
- }
+    // Bring back the 60s!
+    pEnd.setYear(69);
+    System.out.println(p);
+  }
+}
--- JavaSCR/src/ser08j/Period.java
+++ JavaSCR/src/ser08j/Period.java
@@ -1,5 +1,30 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2017 Robert C. Seacord
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package ser08j;
 
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -19,48 +44,109 @@ public final class Period implements Serializable {
    *           if start or end is null
    */
   public Period(Date start, Date end) {
+    // Make defensive copies of each mutable constructor parameter and 
+    // use the copies in place of the originals
     this.start = new Date(start.getTime());
     this.end = new Date(end.getTime());
+    // Throw exception if arguments violate object invariant
     if (this.start.compareTo(this.end) > 0)
-      throw new IllegalArgumentException(start + " after " + end); //$NON-NLS-1$
+      throw new IllegalArgumentException(start + " after " + end);
   }
 
   public Date start() {
+    // Do not return references to private mutable class members
     return new Date(this.start.getTime());
   }
 
   public Date end() {
+    // Do not return references to private mutable class members
     return new Date(this.end.getTime());
   }
 
   @Override
   public String toString() {
-    return this.start + " - " + this.end; //$NON-NLS-1$
+    return this.start + " - " + this.end;
   }
+  
+  @SuppressWarnings("deprecation")
+  public static void main(String[] args) {
+    Date start = new Date();
+    Date end = new Date();
+    Period p = new Period(start, end);
+    Date pEnd = p.end();
+    // Let's turn back the clock
+    pEnd.setYear(78);
+    System.out.println(p);
+    // Bring back the 60s!
+    pEnd.setYear(69);
+    System.out.println(p);
+  } // end main()
+  
+}  // end Serializable class Period
 
-  // readObject method with validity checking - Page 304
-  // This will defend against BogusPeriod attack but not MutablePeriod.
-  // private void readObject(ObjectInputStream s)
-  // throws IOException, ClassNotFoundException {
-  // s.defaultReadObject();
-  //
-  // // Check that our invariants are satisfied
-  // if (start.compareTo(end) > 0)
-  // throw new InvalidObjectException(start +" after "+ end);
-  // }
 
-  // readObject method with defensive copying and validity checking - Page 306
-  // This will defend against BogusPeriod and MutablePeriod attacks.
-  // private void readObject(ObjectInputStream s)
-  // throws IOException, ClassNotFoundException {
-  // s.defaultReadObject();
-  //
-  // // Defensively copy our mutable components
-  // start = new Date(start.getTime());
-  // end = new Date(end.getTime());
-  //
-  // // Check that our invariants are satisfied
-  // if (start.compareTo(end) > 0)
-  // throw new InvalidObjectException(start +" after "+ end);
-  // }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// readObject method with validity checking - Page 304
+// This will defend against BogusPeriod attack but not MutablePeriod.
+// private void readObject(ObjectInputStream s)
+// throws IOException, ClassNotFoundException {
+// s.defaultReadObject();
+//
+// // Check that our invariants are satisfied
+// if (start.compareTo(end) > 0)
+// throw new InvalidObjectException(start +" after "+ end);
+// }
+
+// readObject method with defensive copying and validity checking
+// This will defend against BogusPeriod and MutablePeriod attacks.
+//private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+//  java.io.ObjectInputStream.GetField fields = ois.readFields();
+//
+//  // Defensively copy mutable fields
+//  Date tempDate = ((Date) fields.get("start", new Date())); 
+//  this.start = new Date(tempDate.getTime());
+//  tempDate = ((Date) fields.get("end", new Date())); 
+//  this.end = new Date(tempDate.getTime());
+//
+//  // Check that our invariants are satisfied
+//  if (this.start.compareTo(this.end) > 0)
+//    throw new InvalidObjectException(this.start + " after " + this.end);
+//}
