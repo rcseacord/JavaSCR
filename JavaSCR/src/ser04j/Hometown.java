841d50c7ea0ee70ffa960a13b0ece215aa19a2b6
rcseacord <rcseacord@gmail.com>
2018-04-08 23:39:59 -0400

    restored to first position

--- JavaSCR/src/ser04j/Hometown.java
+++ JavaSCR/src/ser04j/Hometown.java
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2017 Robert C. Seacord
+// Copyright (c) 2018 Robert C. Seacord
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -24,7 +24,6 @@ package ser04j;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -33,73 +32,82 @@ import java.io.Serializable;
 import java.nio.file.AccessDeniedException;
 
 public final class Hometown implements Serializable {
-  private static final long serialVersionUID = 6515419803685137985L;
-  // Private internal state
-  private String town;
-  private static final String MOSCOW = "Moscow"; 
+    private static final long serialVersionUID = 6515419803685137985L;
+    // Private internal state
+    private String town;
+    private static final String MOSCOW = "Moscow";
 
-  private static void validateInput(String newtown) throws AccessDeniedException {
-    if (MOSCOW.equals(newtown)) {
-      throw new AccessDeniedException("Nyet, comrade"); 
+    private static void validateInput(String newtown) throws AccessDeniedException {
+        if (MOSCOW.equals(newtown)) {
+            throw new AccessDeniedException("Nyet, comrade");
+        }
     }
-  }
 
-  public Hometown(String town) throws AccessDeniedException {
-    validateInput(town);
-    // Initialize town to default value
-    this.town = town;
-  }
-
-  // Allows callers to retrieve internal state without security check
-  String getTown() {
-    return this.town;
-  }
-
-  // Privileged callers can modify (private) state
-  public void setTown(String newTown) throws AccessDeniedException {
-    if (!this.town.equals(newTown)) {
-      validateInput(newTown);
-      this.town = newTown;
+    public Hometown(String town) throws AccessDeniedException {
+        validateInput(town);
+        // Initialize town to default value
+        this.town = town;
     }
-  }
 
-  @SuppressWarnings("static-method")
-  private void writeObject(ObjectOutputStream out) throws IOException {
-    System.out.println("writeObject called"); 
-    out.defaultWriteObject();
-  }
-
-  private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-    String readTown;
-    ObjectInputStream.GetField fields = in.readFields();
-    readTown = (String) fields.get("town", null);
-    validateInput(readTown);
-    this.town = readTown;
-  }
-
-  public static void main(String[] args) throws IOException, ClassNotFoundException {
-    // Create Hometown object
-    Hometown ht = new Hometown("Warsaw");
-    System.out.println("Home town is " + ht.getTown());
-    try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("tempdata.ser"))
-    ) {
-      oos.writeObject(ht);
+    // Allows callers to retrieve internal state without security check
+    String getTown() {
+        return this.town;
     }
-    // Construct a new object through deserialization
-    try (
-        // Edit tempdata.ser in %userprofile%\git\JavaSCR
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("tempdata.ser"))
-    ) {
-      ht = (Hometown) ois.readObject();
-    }
-    System.out.println("My town is " + ht.getTown()); 
 
-    // Clean up the file
-    if (!new File("tempdata.ser").delete()) {
-      System.err.println("Failed to delete tempdata.ser");
+    // Privileged callers can modify (private) state
+    public void setTown(String newTown) throws AccessDeniedException {
+        if (!this.town.equals(newTown)) {
+            validateInput(newTown);
+            this.town = newTown;
+        }
     }
-  }
-      }
+
+    @SuppressWarnings("static-method")
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        System.out.println("writeObject called");
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        String readTown;
+        ObjectInputStream.GetField fields = in.readFields();
+        readTown = (String) fields.get("town", null);
+        this.town = readTown;
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        // Create Hometown object
+        Hometown ht = new Hometown("Warsaw");
+        System.out.println("Home town is " + ht.getTown());
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("tempdata.ser"))
+        ) {
+            oos.writeObject(ht);
+        }
+        // Construct a new object through deserialization
+        try (
+                // Edit tempdata.ser in %userprofile%\git\JavaSCR
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream("tempdata.ser"))
+        ) {
+            ht = (Hometown) ois.readObject();
+        }
+        System.out.println("My town is " + ht.getTown());
+
+        // Clean up the file
+        if (!new File("tempdata.ser").delete()) {
+            System.err.println("Failed to delete tempdata.ser");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
